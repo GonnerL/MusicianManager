8d462d2a1e95814b4020dbe714d82e4e0c9de50f
GonnerL <daddy2@ajou.ac.kr>
2019-12-08 17:56:27 +0900

    posting announcement is underway

--- app/src/main/java/com/example/musicianmanager/PostActivity.java
+++ app/src/main/java/com/example/musicianmanager/PostActivity.java
@@ -3,22 +3,24 @@ package com.example.musicianmanager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.SetOptions;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,9 +29,13 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
 
     private FirebaseFirestore mStore = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+    private TimePickerDialog.OnTimeSetListener callbackMethod2;
 
     private String name;
+    private TextView textView_Date, textView_time, textView_eventType;
     private EditText mTitle, mContents;
+    private Spinner spinner_eventType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +44,62 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
 
         mTitle = findViewById(R.id.post_title_edit);
         mContents = findViewById(R.id.post_content_edit);
+        spinner_eventType = findViewById(R.id.spinner_eventType);
+        textView_eventType = findViewById(R.id.textView_eventType);
 
         findViewById(R.id.post_save_button).setOnClickListener(this);
 
-        /*System.out.println("bla");
-        if(mAuth.getCurrentUser()!=null){
-            mStore.collection(FirebaseID.user).document(mAuth.getCurrentUser().getUid())
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            name = task.getResult().getData().get(FirebaseID.name).toString();
-                        }
-                    });
-        }*/
+        this.InitializeView();
+        this.InitializeListener();
 
+        spinner_eventType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                textView_eventType.setText(adapterView.getItemAtPosition(i).toString());
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    public void InitializeView(){
+        textView_Date = findViewById(R.id.textView_date);
+        textView_time = findViewById(R.id.textView_time);
+    }
+
+    public void InitializeListener(){
+        callbackMethod = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                textView_Date.setText(i + "년 " + (i1+1) + "월 " + i2 + "일");
+            }
+        };
+
+        callbackMethod2 = new TimePickerDialog.OnTimeSetListener()
+        {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+            {
+                textView_time.setText(hourOfDay + "시" + minute + "분");
+            }
+        };
+    }
+
+    public void OnClickHandler(View view) {
+        switch(view.getId()){
+            case R.id.button_date:
+                DatePickerDialog dateDialog = new DatePickerDialog(this, callbackMethod, 2019, 7, 18);
+                dateDialog.show();
+                break;
+            case R.id.button_time:
+                TimePickerDialog timeDialog = new TimePickerDialog(this, callbackMethod2, 8, 10, true);
+                timeDialog.show();
+                break;
+        }
     }
 
 
